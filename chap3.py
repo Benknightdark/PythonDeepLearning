9fbae096cfce26c3beb79c694fb9e55409734169
Benknightdark <moon791017@gmail.com>
2018-03-03 22:48:36 +0800

    加入relu

--- chap3.py
+++ chap3.py
@@ -4,6 +4,13 @@ def step_function(x):
     return np.array(x>0,dtype=np.int)
 x=(np.arange(-5.0,5.0,0.1))
 y=step_function(np.arange(-5.0,5.0,0.1))
-plt.plot(x,y)
-plt.ylim(-0.1,1.1)
-plt.show()
+# plt.plot(x,y)
+# plt.ylim(-0.1,1.1)
+# plt.show()
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+def relu(x):
+    return np.maximum(0,x)
+x=np.array([-1.0,1.0,2.0])
+print(sigmoid(x))
+print(relu(x))
