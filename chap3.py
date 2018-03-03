c0ba0f8c60ccfe46a7dfc673a43d7bcae11edb03
Benknightdark <moon791017@gmail.com>
2018-03-03 19:02:10 +0800

    add step function

--- chap3.py
+++ chap3.py
@@ -1,5 +1,9 @@
 import numpy as np
+import matplotlib.pylab as plt
 def step_function(x):
-    y=x>0
-    return y
-print(step_function(1))
+    return np.array(x>0,dtype=np.int)
+x=(np.arange(-5.0,5.0,0.1))
+y=step_function(np.arange(-5.0,5.0,0.1))
+plt.plot(x,y)
+plt.ylim(-0.1,1.1)
+plt.show()
